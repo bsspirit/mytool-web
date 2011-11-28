9853ed56202473b9978ff023c2b9b6105b93ac47
bsspirit <bsspirit@163.com>
2011-11-28 16:43:43 +0800

    a

--- metadata/sql/insert.sql
+++ metadata/sql/insert.sql
@@ -8,12 +8,20 @@ insert into t_website(id,title,url,cid,image) values(4,"Localhost:安吉白茶",
 insert into t_website(id,title,url,cid,image) values(5,"Localhost:奥城兴业","loc.ac-999.com",3,"/images/screen/loc.ac-999.com.png");
 insert into t_website(id,title,url,cid,image) values(6,"Localhost:网站工具","loc.tao6s.com",3,"/images/screen/loc.tao6s.com.png");
 
+insert into t_website(id,title,url,cid,image) values(7,"皮皮书屋","www.ppurl.com",1,"/images/screen/ppurl.com.png");
+insert into t_website(id,title,url,cid,image) values(8,"WebQQ","web.qq.com",1,"/images/screen/web.qq.com.png");
+insert into t_website(id,title,url,cid,image,icon) values(9,"Mahout","mahout.apahce.org",1,"/images/screen/mahout.apache.org.png","/images/icon/mahout.apache.org.ico");
+insert into t_website(id,title,url,cid,image) values(10,"在线词典","dicn.cn",1,"/images/screen/dict.cn.png");
+insert into t_website(id,title,url,cid,image) values(11,"GitHub","github.com",1,"/images/screen/github.com.png");
+
+
 insert into t_website_tag(wid,tag) values(1,"搜索引擎");
 insert into t_website_tag(wid,tag) values(2,"奥城兴业");
 insert into t_website_tag(wid,tag) values(2,"中央空调");
 insert into t_website_tag(wid,tag) values(3,"安吉白茶");
 insert into t_website_tag(wid,tag) values(4,"安吉白茶");
 insert into t_website_tag(wid,tag) values(4,"奥城兴业");
+insert into t_website_tag(wid,tag) values(7,"资源下载");
 
 insert into t_website_catalog(id,name,seq) values(1,"常用站点",1);
 insert into t_website_catalog(id,name,seq) values(2,"我的网站",3);
@@ -25,3 +33,7 @@ insert into t_website_order(wid,score) values(3,99);
 insert into t_website_order(wid,score) values(4,1000);
 insert into t_website_order(wid,score) values(5,999);
 insert into t_website_order(wid,score) values(6,998);
+insert into t_website_order(wid,score) values(7,9);
+insert into t_website_order(wid,score) values(8,8);
+insert into t_website_order(wid,score) values(9,7);
+insert into t_website_order(wid,score) values(10,6);
