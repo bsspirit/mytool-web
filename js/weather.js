f3633a4cc80c3ef7023be2189b2a08fb4d4d9c0e
Conan Zhang <bsspirit@163.com>
2011-12-12 08:20:08 +0800

    'weather'

--- js/weather.js
+++ js/weather.js
@@ -1,29 +1,34 @@
-$(function(){
+//window.onload = weatherHandler;
+window.onload = weatherCallback;
 
-    // Specify the ZIP/location code and units (f or c)
-    var loc = '12578011'; // or e.g. SPXX0050
-    var u = 'c';
+function weatherHandler(){
+	var	loc = 'CHXX0008';//china-weather-location
+    var u = 'c';// units (f or c)
 
-    var query = "SELECT item.condition FROM weather.forecast WHERE location='" + loc + "' AND u='" + u + "'";
+    var query = "SELECT * FROM weather.forecast WHERE location='" + loc + "' AND u='" + u + "'";
     var cacheBuster = Math.floor((new Date().getTime()) / 1200 / 1000);
     var url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + '&format=json&_nocache=' + cacheBuster;
 
-    window['wxCallback'] = function(data) {
-        var info = data.query.results.channel.item.condition;
-        $('#wxIcon').css({
-            backgroundPosition: '-' + (61 * info.code) + 'px 0'
-        }).attr({
-            title: info.text
-        });
-        $('#wxIcon2').append('<img src="http://l.yimg.com/a/i/us/we/52/' + info.code + '.gif" width="34" height="34" title="' + info.text + '" />');
-        $('#wxTemp').html(info.temp + '&deg;' + (u.toUpperCase()));
-    };
-
-    $.ajax({
+	$.ajax({
         url: url,
         dataType: 'jsonp',
         cache: true,
-        jsonpCallback: 'wxCallback'
+        jsonpCallback: 'weatherCallback'
     });
+}
+
+var data = {"query":{	"count":1,"created":"2011-12-11T02:42:41Z","lang":"en-US",	"results":{		"channel":{"title":"Yahoo! Weather - Beijing, CH","link":"http://us.rd.yahoo.com/dailynews/rss/weather/Beijing__CH/*http://weather.yahoo.com/forecast/CHXX0008_c.html","description":"Yahoo! Weather for Beijing, CH","language":"en-us","lastBuildDate":"Sun, 11 Dec 2011 10:00 am CST","ttl":"60","location":{"city":"Beijing","country":"CH","region":""},"units":{"distance":"km","pressure":"mb","speed":"km/h","temperature":"C"},"wind":{"chill":"6","direction":"0","speed":"3.22"},"atmosphere":{"humidity":"34","pressure":"1015.92","rising":"0","visibility":"9.99"},"astronomy":{"sunrise":"7:24 am","sunset":"4:47 pm"},"image":{"title":"Yahoo! Weather","width":"142","height":"18","link":"http://weather.yahoo.com","url":"http://l.yimg.com/a/i/brand/purplelogo//uh/us/news-wea.gif"},"item":{"title":"Conditions for Beijing, CH at 10:00 am CST","lat":"39.93","long":"116.28","link":"http://us.rd.yahoo.com/dailynews/rss/weather/Beijing__CH/*http://weather.yahoo.com/forecast/CHXX0008_c.html","pubDate":"Sun, 11 Dec 2011 10:00 am CST","condition":{"code":"34","date":"Sun, 11 Dec 2011 10:00 am CST","temp":"6","text":"Fair"},"description":"\n<img src=\"http://l.yimg.com/a/i/us/we/52/34.gif\"/><br />\n<b>Current Conditions:</b><br />\nFair, 6 C<BR />\n<BR /><b>Forecast:</b><BR />\nSun - Sunny. High: 8 Low: -5<br />\nMon - Sunny. High: 4 Low: -5<br />\n<br />\n<a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Beijing__CH/*http://weather.yahoo.com/forecast/CHXX0008_c.html\">Full Forecast at Yahoo! Weather</a><BR/><BR/>\n(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)<br/>\n","forecast":[{"code":"32","date":"11 Dec 2011","day":"Sun","high":"8","low":"-5","text":"Sunny"},{"code":"32","date":"12 Dec 2011","day":"Mon","high":"4","low":"-5","text":"Sunny"}],"guid":{"isPermaLink":"false","content":"CHXX0008_2011_12_12_7_00_CST"}}}}}};
+
+function weatherCallback () {
+ 	var today = new Date();
+ 	var results = data.query.results;
+ 	var item = results.channel.item;
     
-});
+    $('#city').html("北京("+item.lat+","+item.long+"), "+$.datepicker.formatDate('yy-mm-dd', today)+":");
+    $('#icon').css({backgroundPosition: '-' + (61 * item.condition.code) + 'px 0'}).attr({title: item.condition.text});
+    $('#icon2').append('<img src="http://l.yimg.com/a/i/us/we/52/' + item.condition.code + '.gif" width="34" height="34" title="' + item.condition.text + '" />');
+    
+    //$('#wxTemp').html(info.temp + '&deg;' + (u.toUpperCase()));
+    
+    $('#create_time').html("发布时间:"+item.pubDate);
+};
--- protected/views/tool/index.php
+++ protected/views/tool/index.php
@@ -1,12 +1,35 @@
 <h1>天气预报</h1>
 <div class="view">
-	<div id="wxWrap">
-	    <span id="wxIntro">
-	        Currently in Manhattan, NY: 
-	    </span>
-	    <span id="wxIcon2"></span>
+	<div id="weather">
+	    <span id="city"></span>
+	    <span id="icon"></span>
+	    <span id="icon2"></span>
 	    <span id="wxTemp"></span>
+	    <div id="create_time"></div>
 	</div>
+	
+	<div><div style="padding-top: 2px;" class="modlabel"><a onclick="m_74_navigateToCanvas(0);return false;" class="w_lnk" href="#">
+	北京, 北京市</a></div>
+	<div class="w_box">
+		<table width="100%" cellspacing="0" cellpadding="0" border="0">
+			<tbody>
+				<tr><td>
+					<div class="w_ccs">
+						<div class="w_ccis"><img width="40" height="40" alt="Clear" src="//g0.gstatic.com/ig/images/weather/sunny.png" id="w_74_c0_icon" class="w_cci"><br></div>
+						<div id="w_74_c0_temp" class="w_cc_temp">6°C</div>
+						<div id="w_74_c0_text" style="" class="w_cc_text">Current:&nbsp;Clear<br>Wind: N at 3&nbsp;km/h<br>Humidity: 34%</div>
+					</div>
+				</td></tr>
+				<tr><td>
+					<div id="w_74_c0_fcs" style="width: 100%;" class="w_fcs">
+						<div title="Mostly Sunny" class="w_fc">Sun<br><div class="w_fcid"><img width="40" height="40" alt="Mostly Sunny" src="//g0.gstatic.com/ig/images/weather/mostly_sunny.png" class="w_fci"></div>
+						<nobr>-4°&nbsp;|&nbsp;9°</nobr></div><div title="Fog" class="w_fc">Mon<br><div class="w_fcid"><img width="40" height="40" alt="Fog" src="//g0.gstatic.com/ig/images/weather/fog.png" class="w_fci"></div>
+						<nobr>-5°&nbsp;|&nbsp;6°</nobr></div><div title="Mostly Sunny" class="w_fc">Tue<br><div class="w_fcid"><img width="40" height="40" alt="Mostly Sunny" src="//g0.gstatic.com/ig/images/weather/mostly_sunny.png" class="w_fci"></div>
+						<nobr>-7°&nbsp;|&nbsp;5°</nobr></div><div title="Clear" class="w_fc">Wed<br><div class="w_fcid"><img width="40" height="40" alt="Clear" src="//g0.gstatic.com/ig/images/weather/sunny.png" class="w_fci"></div>
+						<nobr>-8°&nbsp;|&nbsp;-1°</nobr></div></div>
+				</td></tr></tbody>
+		</table>
+	</div></div>
 </div>
 
 <link rel="stylesheet" href="/css/weather.css" />
