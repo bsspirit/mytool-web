f5e2efc59a17be45e6aeb01a1465f1da05b0e737
Conan Zhang <bsspirit@163.com>
2011-12-13 18:02:55 +0800

    a

--- js/dict.js
+++ js/dict.js
@@ -21,11 +21,11 @@ function function_dist_tag(obj){
 		html += '<div class="title">';
 		html += '<span class="name w200">'+v.name+'</span>';
 		html += '<a href="javascript:void(0);" onclick="click_dict_tagword_add('+v.id+')"><span class="act">Add</span></a>';
-		html += '<a href="javascript:void(0);" onclick="click_dict_tagword_cancel('+v.id+')"><span class="act">Cancel</span></a>';
 		html += '<form id="form_dict_'+v.id+'" class="form">'
 		html += '<input type="text" class="w200" name="dict_word"/>';
 		html += '<a href="javascript:void(0);" onclick="submit_dict_tagword_add('+v.id+')"><span class="act">Add</span></a>';
 		html += '<a href="javascript:void(0);" onclick="submit_dict_tagword_del('+v.id+')"><span class="act">Delete</span></a>';
+		html += '<a href="javascript:void(0);" onclick="click_dict_tagword_cancel('+v.id+')"><span class="act">Cancel</span></a>';
 		html += '<input type="hidden" name="dict_tag" value="'+v.id+'"/>';
 		html += '</form>';
 		html += '</div>';
