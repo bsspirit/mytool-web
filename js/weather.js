6a33b15522533f8579c5f516c58672fce3e5b7ec
Conan Zhang <bsspirit@163.com>
2011-12-16 22:37:39 +0800

    'weather'

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,10 @@
-//window.onload = weatherHandler;
-window.onload = weatherCallback;
+window.onload = weatherHandler;
 
-function weatherHandler(){
+function weatherHandler(code){
 	var	loc = 'CHXX0008';//china-weather-location
+	if(code!=undefined && code!=null && code.length>0){
+		loc = code;
+	}
     var u = 'c';// units (f or c)
 
     var query = "SELECT * FROM weather.forecast WHERE location='" + loc + "' AND u='" + u + "'";
@@ -17,42 +19,39 @@ function weatherHandler(){
     });
 }
 
-var data = {"query":{	"count":1,"created":"2011-12-11T02:42:41Z","lang":"en-US",	"results":{		"channel":{"title":"Yahoo! Weather - Beijing, CH","link":"http://us.rd.yahoo.com/dailynews/rss/weather/Beijing__CH/*http://weather.yahoo.com/forecast/CHXX0008_c.html","description":"Yahoo! Weather for Beijing, CH","language":"en-us","lastBuildDate":"Sun, 11 Dec 2011 10:00 am CST","ttl":"60","location":{"city":"Beijing","country":"CH","region":""},"units":{"distance":"km","pressure":"mb","speed":"km/h","temperature":"C"},"wind":{"chill":"6","direction":"0","speed":"3.22"},"atmosphere":{"humidity":"34","pressure":"1015.92","rising":"0","visibility":"9.99"},"astronomy":{"sunrise":"7:24 am","sunset":"4:47 pm"},"image":{"title":"Yahoo! Weather","width":"142","height":"18","link":"http://weather.yahoo.com","url":"http://l.yimg.com/a/i/brand/purplelogo//uh/us/news-wea.gif"},"item":{"title":"Conditions for Beijing, CH at 10:00 am CST","lat":"39.93","long":"116.28","link":"http://us.rd.yahoo.com/dailynews/rss/weather/Beijing__CH/*http://weather.yahoo.com/forecast/CHXX0008_c.html","pubDate":"Sun, 11 Dec 2011 10:00 am CST","condition":{"code":"34","date":"Sun, 11 Dec 2011 10:00 am CST","temp":"6","text":"Fair"},"description":"\n<img src=\"http://l.yimg.com/a/i/us/we/52/34.gif\"/><br />\n<b>Current Conditions:</b><br />\nFair, 6 C<BR />\n<BR /><b>Forecast:</b><BR />\nSun - Sunny. High: 8 Low: -5<br />\nMon - Sunny. High: 4 Low: -5<br />\n<br />\n<a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Beijing__CH/*http://weather.yahoo.com/forecast/CHXX0008_c.html\">Full Forecast at Yahoo! Weather</a><BR/><BR/>\n(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)<br/>\n","forecast":[{"code":"32","date":"11 Dec 2011","day":"Sun","high":"8","low":"-5","text":"Sunny"},{"code":"32","date":"12 Dec 2011","day":"Mon","high":"4","low":"-5","text":"Sunny"}],"guid":{"isPermaLink":"false","content":"CHXX0008_2011_12_12_7_00_CST"}}}}}};
-
-function weatherCallback () {
+function weatherCallback (data) {
  	var today = new Date();
  	var results = data.query.results;
  	var item = results.channel.item;
+ 	var location = results.channel.location;
+ 	var link = results.channel.link;
  	
  	var txt = '<div class="current">';
- 		txt += '<div class="loc l">北京</div>';
- 		txt += '<div class="info">'+$.datepicker.formatDate('yy-mm-dd', today)+','+item.condition.text+','+item.condition.temp+'°C</div>';
- 		txt += '<div class="info">('+item.lat+','+item.long+'),风力:4</div>';
+ 		txt += '<div class="loc">'+location.city+'&nbsp;&nbsp;<span class="red">'+item.condition.temp+'°C</span></div>';
+ 		txt += '<div class="info">('+item.lat+','+item.long+'),&nbsp;&nbsp;'+item.condition.text;
+ 		txt += '</div>';
  		txt += '</div>';
  		txt += '<div class="future">';
  		txt += '<ul>';
- 		txt += '<li>2011-11-12,睛,6C|12C</li>';
- 		txt += '<li>2011-11-13,睛,6C|12C</li>';
- 		txt += '<li>2011-11-14,睛,6C|12C</li>';
+ 		$.each(item.forecast,function(k,v){
+ 			txt += '<li>'+$.datepicker.formatDate('yy-mm-dd', new Date(v.date));
+ 			txt += ',&nbsp;&nbsp;'+v.day;
+ 			txt += ',&nbsp;&nbsp;'+v.text;
+ 			txt += ',&nbsp;&nbsp;'+v.high+'°C|'+v.low+'°C</li>';
+ 		});
  		txt += '</ul>';
  		txt += '</div>';
  		txt += '<div class="addr">';
  		txt += '<ul>';
- 		txt += '<li>上海</li>';
- 		txt += '<li>广州</li>';
- 		txt += '<li>天津</li>';
- 		txt += '<li>重庆</li>';
+ 		txt += '<li><a href="javascript:void(0);" onclick=weatherHandler("CHXX0008")>Beijing</a></li>';
+ 		txt += '<li><a href="javascript:void(0);" onclick=weatherHandler("CHXX0116")>ShangHai</a></li>';
+ 		txt += '<li><a href="javascript:void(0);" onclick=weatherHandler("CHXX0037")>GuangZhou</a></li>';
+ 		txt += '<li><a href="javascript:void(0);" onclick=weatherHandler("CHXX0017")>ChongQing</a></li>';
  		txt += '</ul>';
  		txt += '</div>';
  		txt += '<div class="version">';
- 		txt += '发布日期:'+$.datepicker.formatDate('yy-mm-dd', new Date(item.pubDate))+'. Yahoo Weather.';
- 		txt += '</div>';
- 	
+ 		txt += 'Power by <a target="_blank" href="'+link.substring(link.charAt('*'))+'">Yahoo Weather</a>.';
+ 		txt += '</div>';	
+ 		
  	$('#weather').html(txt);
-    
-//    $('#city').html("北京("+item.lat+","+item.long+"), "+$.datepicker.formatDate('yy-mm-dd', today)+":");
-//    $('#icon').css({backgroundPosition: '-' + (61 * item.condition.code) + 'px 0'}).attr({title: item.condition.text});
-//    $('#icon2').append('<img src="http://l.yimg.com/a/i/us/we/52/' + item.condition.code + '.gif" width="34" height="34" title="' + item.condition.text + '" />');
-//    $('#icon3').append(item.condition.temp+"°C"+","+item.condition.text);
-//    $('#create_time').html("发布时间:"+item.pubDate);
 };
