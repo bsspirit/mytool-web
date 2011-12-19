e926d2244f615363d72546ed29c4cf8a3f1ed712
Conan Zhang <bsspirit@163.com>
2011-12-19 09:41:23 +0800

    json

--- js/jsonFormat.js
+++ js/jsonFormat.js
@@ -70,7 +70,7 @@ function style_prop(k){
 function style_val(v){
 	var val= "";
 	if(typeof(v) == 'string'){
-		val='<span class="str">\"'+v+'\"</span>';
+		val='<span class="str">\"'+HTMLEnCode(v)+'\"</span>';
 	} else if (typeof(v) == 'number'){
 		val='<span class="num">'+v+'</span>';
 	} else if (typeof(v) == 'boolean'){
@@ -78,3 +78,28 @@ function style_val(v){
 	}
 	return val;
 }
+
+function HTMLEnCode(str){   
+      var s ="";   
+      if(str.length == 0) return "";   
+      s=str.replace(/&/g,"&gt;");   
+      s=s.replace(/</g,"&lt;");   
+      s=s.replace(/>/g,"&gt;");   
+      s=s.replace(/ /g,"&nbsp;");   
+      s=s.replace(/\'/g,"'");   
+      s=s.replace(/\"/g,"&quot;");   
+      //s=s.replace(/\n/g,"&lt;br&nbsp;/&gt;");   
+      return    s;   
+}   
+function HTMLDeCode(str){   
+      var s="";   
+      if(str.length == 0) return "";   
+      s=str.replace(/&gt;/g,"&");   
+      s=s.replace(/&lt;/g,"<");   
+      s=s.replace(/&gt;/g,">");   
+      s=s.replace(/&nbsp;/g," ");   
+      s=s.replace(/'/g,"\'");   
+      s=s.replace(/&quot;/g,"\"");   
+      s=s.replace(/<br>/g,"\n");   
+      return s;   
+}   
